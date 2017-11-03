c0470075d2d0fc951333681757d662dab46be9e1
Jaco Greeff <jaco@parity.io>
2017-11-03 09:35:08 +0100

    Rebuild with updated dependencies

--- src/store.js
+++ src/store.js
@@ -303,7 +303,7 @@ export default class Store {
 
   createVault () {
     if (this.vaultNameError || this.vaultPasswordRepeatError) {
-      return Promise.reject();
+      return Promise.reject(new Error('Unable to create vault'));
     }
 
     this.setBusyCreate(true);
